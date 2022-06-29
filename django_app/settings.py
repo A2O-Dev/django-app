0132866bb63d7cd6038fcaffa696f7df2c77ccda
David Almaraz <david.almaraz@a2odev.com>
2022-06-29 18:56:16 -0400

    Add Authentication token

--- django_app/settings.py
+++ django_app/settings.py
@@ -39,8 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'product_api'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- django_app/urls.py
+++ django_app/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.urls import path, include
 from product_api import urls as product_urls
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('api/auth/', include('rest_framework.urls')),
+    path('oauth/token/', obtain_auth_token),
     path('api/products/', include(product_urls))
 ]
