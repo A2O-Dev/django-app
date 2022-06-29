676ad3ad6ec54ebe0cb5997b3f2b5602fa68d76c
David Almaraz <david.almaraz@a2odev.com>
2022-06-29 16:04:27 -0400

    Product migration

--- product_api/models.py
+++ product_api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=20, unique=True)
+    price = models.FloatField()
+    dimensions = models.TextField(null=True, blank=True)
+    colors = models.TextField(null=True, blank=True)
+    tags = models.TextField(null=True, blank=True)
+    stock = models.PositiveSmallIntegerField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    class Meta:
+        db_table = 'products'
+
+    def __str__(self):
+        return self.name
