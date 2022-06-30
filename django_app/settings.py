0e15f52d403e742614ef6bccf655ae3bd16e0926
David Almaraz <david.almaraz@a2odev.com>
2022-06-30 09:28:09 -0400

    Adding pagination

--- django_app/settings.py
+++ django_app/settings.py
@@ -47,7 +47,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication'
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5
 }
 
 MIDDLEWARE = [
